0cda443f8e6f63db519f407f742e6d729e14bc96
Sergio Pimienta <pimienta.sergio@gmail.com>
2016-05-27 22:34:41 -0500

    h

--- app/src/main/java/mx/iteso/sergio/emt_ad/Analisis.java
+++ app/src/main/java/mx/iteso/sergio/emt_ad/Analisis.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -13,6 +15,8 @@ import android.view.ViewGroup;
  */
 public class Analisis extends Fragment {
 
+
+    private View GlobalView;
 
     public Analisis() {
         // Required empty public constructor
@@ -22,8 +26,30 @@ public class Analisis extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        GlobalView = inflater.inflate(R.layout.fragment_analisis, container, false);
+
+        populateListView();
+
+
+
+
+        return GlobalView;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analisis, container, false);
+        //return inflater.inflate(R.layout.fragment_analisis, container, false);
+    }
+
+    private void populateListView() {
+        String [] myItems = {"Aún no has agendado alguna visita :("};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                myItems);
+
+        ListView list = (ListView) GlobalView.findViewById(R.id.listViewAnalisis);
+        list.setAdapter(adapter);
+
     }
 
 }
