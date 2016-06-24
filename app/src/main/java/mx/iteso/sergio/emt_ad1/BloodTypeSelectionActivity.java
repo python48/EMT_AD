bdaa1c9ce66b3197bb93c24620a06891e5b98fcd
Sergio Pimienta <pimienta.sergio@gmail.com>
2016-06-23 22:34:06 -0500

    l
    ele..

--- app/src/main/java/mx/iteso/sergio/emt_ad1/BloodTypeSelectionActivity.java
+++ app/src/main/java/mx/iteso/sergio/emt_ad1/BloodTypeSelectionActivity.java
@@ -1,23 +1,32 @@
 package mx.iteso.sergio.emt_ad1;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import mx.iteso.sergio.emt_ad1.R;
 
 public class BloodTypeSelectionActivity extends AppCompatActivity {
 
     ///Buttons
-    Button buttonA;
-    Button buttonAB;
-    Button buttonB;
-    Button buttonO;
-    Button buttonPlus;
-    Button buttonMinus;
+    private Button buttonA;
+    private Button buttonAB;
+    private Button buttonB;
+    private Button buttonO;
+    private Button buttonPlus;
+    private Button buttonMinus;
+
+    TextView sangreSeleccionada;
+
+    String selection = "+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +39,80 @@ public class BloodTypeSelectionActivity extends AppCompatActivity {
         buttonPlus = (Button) findViewById(R.id.boton_sangre_plus);
         buttonMinus = (Button) findViewById(R.id.boton_sangre_minus);
 
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        //sangreSeleccionada.setText("N/D");
+
+
         setContentView(R.layout.activity_blood_type_selection);
     }
 
 
     public void buttonAClicked(View view)
     {
-        //buttonA.setBackgroundDrawable(getResources().getDrawable(R.drawable.my_shape_alternate));
+        String sign = selection.substring(selection.length()-1,selection.length());
+        selection = "A" + sign;
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        sangreSeleccionada.setText(selection);
+    }
+    public void buttonBClicked(View view)
+    {
+        String sign = selection.substring(selection.length()-1,selection.length());
+        selection = "B" + sign;
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        sangreSeleccionada.setText(selection);
+    }
+    public void buttonABClicked(View view)
+    {
+        String sign = selection.substring(selection.length()-1,selection.length());
+        selection = "C" + sign;
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        sangreSeleccionada.setText("AB"+sign);
+    }
+    public void buttonOClicked(View view)
+    {
+        String sign = selection.substring(selection.length()-1,selection.length());
+        selection = "O" + sign;
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        sangreSeleccionada.setText(selection);
+    }
+
+
+    public void buttonPlusClicked(View view)
+    {
+        String letter = selection.substring(0,selection.length()-1);
+        selection = letter + "+";
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        sangreSeleccionada.setText(selection);
+    }
+    public void buttonMinusClicked(View view)
+    {
+        String letter = selection.substring(0,selection.length()-1);
+        selection = letter + "-";
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        sangreSeleccionada.setText(selection);
+    }
+
+
+    public void guardarCambioClicked(View view)
+    {
+        sangreSeleccionada = (TextView) findViewById(R.id.textViewSangre);
+        String sangre = selection;
+        if (sangre.length()<=2)
+        {
+            new AlertDialog.Builder(view.getContext())
+                    .setTitle("Alerta")
+                    .setMessage("El tipo de sangre no quedo bien definido")
+                    .show();
+            RegistroFragment.Sangre = "N/D";
+        }
+        else
+        {
+            RegistroFragment.Sangre = sangre;
+            Intent intent = new Intent(this, Registro2.class);
+            startActivity(intent);
+        }
+
+
     }
 
 }
